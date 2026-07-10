b8a51efe06415433e26e69a13813a35eb8f52219
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-10 10:32:18 -0500

    Commit #17

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,2 +1,46 @@
 /* src/usuarios/dto/create-usuario.dto.ts: */
-export class CreateUsuarioDto { }
+import { IsEmail, IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, } from 'class-validator';
+import { Genero } from '../../generated/prisma/enums';
+
+export class CreateUsuarioDto {
+    @IsString()
+    @IsNotEmpty()
+    nombre: string;
+
+    @IsInt()
+    edad: number;
+
+    @IsOptional()
+    @IsString()
+    biografia?: string;
+
+    @IsOptional()
+    @IsNumber()
+    peso?: number;
+
+    @IsOptional()
+    @IsNumber()
+    altura?: number;
+
+    @IsString()
+    @IsNotEmpty()
+    nacionalidad: string;
+
+    @IsEnum(Genero)
+    genero: Genero;
+
+    @IsString()
+    @IsNotEmpty()
+    ciudad: string;
+
+    @IsString()
+    @IsNotEmpty()
+    pais: string;
+
+    @IsString()
+    @IsNotEmpty()
+    numeroTelefono: string;
+
+    @IsEmail()
+    correo: string;
+}
