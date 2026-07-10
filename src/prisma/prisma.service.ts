dce146788bc585bb2d8c5b782bac32c774c16b73
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-10 10:19:18 -0500

    Commit #15

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,25 @@
 /* src/prisma/prisma.service.ts: */
+import 'dotenv/config';
 import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+
+function obtenerUrlBaseDatos(): string {
+    const urlBaseDatos = process.env['DATABASE_URL'];
+    if (!urlBaseDatos) {
+        throw new Error(
+            'La variable de entorno DATABASE_URL no está definida en el archivo .env.',
+        );
+    }
+    return urlBaseDatos;
+}
 
 @Injectable()
-export class PrismaService { }
+export class PrismaService extends PrismaClient {
+    constructor() {
+        const adapter = new PrismaPg({
+            connectionString: obtenerUrlBaseDatos(),
+        });
+        super({ adapter });
+    }
+}
