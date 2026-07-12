1129bc8a66e0f0143077b57556318461baf2881e
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-12 18:37:14 -0500

    Commit #107

--- prisma.config.usuarios.ts
+++ prisma.config.usuarios.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.usuarios.prisma',
+  schema: 'prisma/schema-usuarios.prisma',
   migrations: {
     path: 'prisma/migrations-usuarios',
   },
