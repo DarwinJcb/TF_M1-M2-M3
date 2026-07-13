e48c356a0c288d8a83a24623c2555f3d33ce3879
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-13 03:48:55 -0500

    Commit #140

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,8 @@
 /* src/auth/auth.module.ts: */
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Module({})
+@Module({
+  providers: [AuthService]
+})
 export class AuthModule { }
