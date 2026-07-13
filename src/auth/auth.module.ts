7005b50c9c7af542192a7d96fedec412261c98da
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-13 04:29:07 -0500

    Commit #151

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,39 @@
 /* src/auth/auth.module.ts: */
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { PrismaUsuariosModule } from '../prisma-usuarios/prisma-usuarios.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+function obtenerSecretoJwt(): string {
+    const secretoJwt = process.env['JWT_SECRET'];
+
+    if (!secretoJwt) {
+        throw new Error(
+            'La variable de entorno JWT_SECRET no está definida en el archivo .env.',
+        );
+    }
+
+    return secretoJwt;
+}
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController],
+    imports: [
+        PrismaUsuariosModule,
+        PassportModule.register({
+            defaultStrategy: 'jwt',
+        }),
+        JwtModule.register({
+            secret: obtenerSecretoJwt(),
+            signOptions: {
+                expiresIn: '1h',
+            },
+        }),
+    ],
+    providers: [AuthService],
+    controllers: [AuthController],
+    exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
